40f6d8c51b57746010b4815afc2c8059df8ae0f2
Francisco Lopez <francisco.lopez@cytosmart.com>
2023-08-23 10:06:34 +0200

    configure azure app logging

--- src/AzureAppLogging/Program.cs
+++ src/AzureAppLogging/Program.cs
@@ -1,14 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.AzureApp()
+    .CreateBootstrapLogger();
 
-var app = builder.Build();
+Log.Information("Starting Application");
 
-app.MapGet("/", () => "Hello World");
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-}
+    var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+    builder.Host
+        .UseSerilog((context, loggerConfiguration) => loggerConfiguration
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+            .ReadFrom.Configuration(context.Configuration));
+
+    var app = builder.Build();
+
+    app.MapGet("/", () => "Hello World");
+
+    if (app.Environment.IsDevelopment())
+    {
+    }
+
+    app.Run();
+}
+catch (Exception exception) when (exception.GetType().Name is not "StopTheHostException" &&
+                                  exception.GetType().Name is not "HostAbortedException")
+{
+    Log.Fatal(exception, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
