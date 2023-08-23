070f35417fc8d4064a998a451bb19f2be0263e1d
Francisco Lopez <francisco.lopez@cytosmart.com>
2023-08-23 09:29:20 +0200

    added azure app logging project

---
+++ src/AzureAppLogging/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+}
+
+app.Run();
